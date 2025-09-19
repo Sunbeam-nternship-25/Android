8bfc72f14e2fff5224be3f016e73d81d9eb5d568
Vaishnavi Dayma <vaishnavidayma3008@gmail.com>
2025-09-19 12:22:55 +0530

    basic login code added and login sucessfully

--- app/src/main/java/com/vaishnavidayma/studentfeedback/activity/MainActivity.java
+++ app/src/main/java/com/vaishnavidayma/studentfeedback/activity/MainActivity.java
@@ -17,6 +17,9 @@ import androidx.core.view.WindowInsetsCompat;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.gson.JsonObject;
 import com.vaishnavidayma.studentfeedback.R;
+import com.vaishnavidayma.studentfeedback.entity.Student;
+import com.vaishnavidayma.studentfeedback.utils.Constants;
+import com.vaishnavidayma.studentfeedback.utils.RetrofitClient;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -24,17 +27,42 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextInputEditText editTextEmail, editTextPassword;
+    TextInputEditText editTextEmail,editTextPassword;
     TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextEmail= findViewById(R.id.editTextEmaill);
+        editTextPassword= findViewById(R.id.editTextPassword);
+        textView= findViewById(R.id.textView4);
 
+      
 
     }
 
-}
 
-  
+
+
+    public void login(View view){
+        Student student = new Student();
+        student.setEmail(editTextEmail.getText().toString());
+        student.setPassword(editTextPassword.getText().toString());
+        RetrofitClient.getInstance().getApi().loginStudent(student).enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                if(response.body().get("status").getAsString().equals("success")){
+                    Toast.makeText(MainActivity.this, "Login SucessFully",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable throwable) {
+                Log.e("LoginError", "API call failed", throwable);
+                Toast.makeText(MainActivity.this, "Something Went Wrong",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
