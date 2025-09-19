b062d34befad95eb1e5d7e3a4f1cd9b537831114
Vaishnavi Dayma <vaishnavidayma3008@gmail.com>
2025-09-19 12:02:48 +0530

    Android app created login UI created

---
+++ app/src/main/java/com/vaishnavidayma/studentfeedback/activity/MainActivity.java
@@ -0,0 +1,41 @@
+package com.vaishnavidayma.studentfeedback.activity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.gson.JsonObject;
+import com.vaishnavidayma.studentfeedback.R;
+import com.vaishnavidayma.studentfeedback.entity.Student;
+import com.vaishnavidayma.studentfeedback.utils.Constants;
+import com.vaishnavidayma.studentfeedback.utils.RetrofitClient;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText editTextEmail,editTextPassword;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+       
+    }
+
+  
