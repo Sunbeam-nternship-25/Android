c2ac2091e728232d5ffa9011d15aa4551fe854e3
Vaishnavi Dayma <vaishnavidayma3008@gmail.com>
2025-09-22 12:02:53 +0530

    For Every UI element findViewById completed

--- app/src/main/java/com/vaishnavidayma/studentfeedback/activity/FeedbackActivity.java
+++ app/src/main/java/com/vaishnavidayma/studentfeedback/activity/FeedbackActivity.java
@@ -1,9 +1,14 @@
 package com.vaishnavidayma.studentfeedback.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -12,10 +17,39 @@ import com.vaishnavidayma.studentfeedback.R;
 
 public class FeedbackActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
+    TextView textView1,textView2,textView3,textView4;
+
+    RadioGroup radioGroupQ1;
+    RadioGroup radioGroupQ2;
+    RadioGroup radioGroupQ3;
+    RadioGroup radioGroupQ4;
+    RadioGroup radioGroupQ5;
+
+    EditText etComments;
+
+    View feedbackForm;
+
+    TextView messageText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        textView1= findViewById(R.id.textView1);
+        textView2= findViewById(R.id.textView2);
+        textView3= findViewById(R.id.textView3);
+        textView4= findViewById(R.id.textView4);
+        radioGroupQ1= findViewById(R.id.radioGroupQ1);
+        radioGroupQ2= findViewById(R.id.radioGroupQ2);
+        radioGroupQ3= findViewById(R.id.radioGroupQ3);
+        radioGroupQ4= findViewById(R.id.radioGroupQ4);
+        radioGroupQ5= findViewById(R.id.radioGroupQ5);
+        etComments = findViewById(R.id.etComments);
+        feedbackForm = findViewById(R.id.feedbackForm);
+        messageText= findViewById(R.id.messageText);
 
     }
 }
