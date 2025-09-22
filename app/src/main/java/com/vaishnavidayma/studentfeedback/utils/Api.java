b68a19ffeb968c151b01c94d413aa3cf4c44fd7f
Vaishnavi Dayma <vaishnavidayma3008@gmail.com>
2025-09-22 10:00:00 +0530

    created the method for all API's

--- app/src/main/java/com/vaishnavidayma/studentfeedback/utils/Api.java
+++ app/src/main/java/com/vaishnavidayma/studentfeedback/utils/Api.java
@@ -11,18 +11,33 @@ import retrofit2.http.POST;
 
 public interface Api {
 
-    public static final String BASEURL = "http://192.168.1.100:4000";
+    public static final String BASEURL = "http://192.168.1.102:4000";
     @POST ("/student/login")
     public Call<JsonObject> loginStudent(@Body Student student);
-
-    @GET ("/course/allCourses")
-    public Call<JsonObject> getAllCourses();
-
 
     @POST ("/student/newRegister")
     public Call<JsonObject> registerStudent(@Body Student student);
 
+    @GET ("/course/allCourses")
+    public Call<JsonObject> getAllCourses();
 
     @POST("/group/groupbycourse/course_name")
     Call<JsonObject> getGroupsByCourse(@Body JsonObject courseName);
+
+    @GET("/feedback/activeFeedback")
+    Call<JsonObject> getActiveFeedback(
+            @Header("Authorization") String token
+    );
+
+
+    @POST("/feedback/fillFeedback2")
+    Call<JsonObject> submitFeedback(@Header("Authorization") String token, @Body JsonObject body);
+
+    @GET("/feedback/checkFeedback2")
+    Call<JsonObject> checkFeedback(@Header("Authorization") String token);
+
+    @GET("/student/studentbyid")
+    Call<JsonObject> getStudentById(@Header("Authorization") String token);
+
+
 }
